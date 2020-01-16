6cfa41be369e6bc80766ea6ed096e0b511f44852
dengbb <804433988@qq.com>
2020-01-16 09:46:31 +0900

    update kline

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ import requests
 import json
 import datetime
 import time
+import os
 
-symbolList = ["BTC_CW","EOS_CW","BCH_CW","BSV_CW","ETC_CW","ETH_CW","XRP_CW","TRX_CW"]
+
+symbolList = ["BTC_CW"]#,"EOS_CW","BCH_CW","BSV_CW","ETC_CW","ETH_CW","XRP_CW","TRX_CW"]
+today =  str(datetime.date.today())
+os.mkdir(today)
+pwd = os.path.dirname(os.path.realpath(__file__)) +"/" + today
 for j in ["1min","5min","15min","30min","60min"]:
     for i in symbolList:
         url = "http://api.hbdm.com/market/history/kline?period=" + j + "&size=200&symbol="+i
-        today =  datetime.date.today()
+        
         t = requests.get(url).json()
-        name = i+"_"+j+"_"+str(today)+".json"
-        # name = "./" + j +"/"+ name
-        with open(name, 'w') as outfile:
+        name = i+"_"+j+"_"+today+".json"
+        with open(os.path.join(pwd,name), 'w') as outfile:
             json.dump(t, outfile)
     time.sleep(2)

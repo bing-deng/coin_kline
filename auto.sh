289a1af8d974812edcbe610e54954982f70f78bd
fun <804433988@qq.com>
2020-01-24 03:10:55 +0000

    update kline

--- auto.sh
+++ auto.sh
@@ -1,6 +1,2 @@
-cd /root/coin_kline
-python3 app.py
-
-git add .
-git commit -m "update kline"
-git push
+python3 /root/coin_kline/app.py
+cd /root/coin_kline && git add .  && git commit -m "update kline" && git push
